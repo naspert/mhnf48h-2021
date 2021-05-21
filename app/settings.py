ff8adc16d06d22eb1134ec15af510ec07d66ac3e
Nicolas Aspert <nicolas.aspert@epfl.ch>
2021-05-21 21:39:54 +0200

    remove ssl for db

--- app/settings.py
+++ app/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     }
 }
 # Change 'default' database configuration with $DATABASE_URL.
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=False))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
