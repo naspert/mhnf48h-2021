51443865b7308bab82ea633331da82d4dc70c0dc
Nicolas Aspert <nicolas.aspert@epfl.ch>
2021-05-23 15:44:59 +0200

    csv export

--- field/urls.py
+++ field/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', forms.ObservationCreate.as_view(), name='observ_create'),
     path('update/<int:pk>/', forms.ObservationUpdate.as_view(), name='observ_update'),
     path('delete/<int:pk>/', forms.ObservationDelete.as_view(), name='observ_delete'),
+    path('all/', views.show_all, name='show_all'),
+    path('csv-all/', views.ObservationExportView.as_view(), name='csv_export_all')
 ]
--- field/views.py
+++ field/views.py
@@ -1,5 +1,9 @@
+import csv
 from django.shortcuts import render
+from django.http import HttpResponse, StreamingHttpResponse
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from csv_export.views import CSVExportView
 from .models import Observation
 
 
@@ -7,6 +11,21 @@ from .models import Observation
 def home(request):
     user_obs = Observation.objects.filter(owner=request.user)
     return render(request, 'field/home.html', {'observations': user_obs})
+
+
+@login_required
+def show_all(request):
+    obs = Observation.objects.all()
+    return render(request, 'field/all.html', {'observations': obs})
+
+
+@method_decorator(login_required, name='get')
+class ObservationExportView(CSVExportView):
+    model = Observation
+    fields = ('owner__username', 'species', 'remarks', 'created_date', 'last_modified_date')
+
+
+
 
 
 
